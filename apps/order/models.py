1840cd56936722e3cd746f43fe1db35fa6ce3779
quannguyen <waynenguyenus@gmail.com>
2021-09-03 22:23:08 -0700

    first upload

---
+++ apps/order/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+from apps.product.models import Product
+from apps.vendor.models import Vendor
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    paid_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    vendors = models.ManyToManyField(Vendor, related_name='orders')
+
+    class Meta:
+        ordering = ['-created_at']
+        #( make order date always to top of list )
+    
+    def __str__(self):
+        return self.first_name
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='items', on_delete=models.CASCADE)
+    vendor = models.ForeignKey(Vendor, related_name='items', on_delete=models.CASCADE)
+    vendor_paid = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+    date = models.DateTimeField(auto_now_add=True)
+
+    
+    def __str__(self):
+        return '%s' % self.id
+    
+    def get_total_price(self):
+        return self.price * self.quantity
---
+++ apps/vendor/urls.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.vendors, name='vendors'),
+    path('become-vendor/', views.become_vendor, name='become_vendor'),
+    path('vendor-admin/', views.vendor_admin, name='vendor_admin'),
+    path('edit-vendor/', views.edit_vendor, name='edit_vendor'),
+    path('<int:vendor_id>/', views.vendor, name='vendor'),
+    path('<int:pk>/delete', views.delete_vendor, name='delete_vendor'),
+    path('add-product/', views.add_product, name='add_product'),
+    path('edit-product/<int:pk>', views.edit_product, name='edit_product'),
+    path('delete-product/<int:pk>', views.delete_product, name='delete_product'),
+
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='vendor/login.html'), name='login'),
+
+
+]
