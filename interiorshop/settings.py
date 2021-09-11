b0d2254131ebf42f02ccb0be9b974d6c12802258
quannguyen <waynenguyenus@gmail.com>
2021-09-11 14:17:33 -0700

    555

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -15,9 +15,9 @@ import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
