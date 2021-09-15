2ea2abca4851d8edb702d1aabc706835679e9099
quannguyen <waynenguyenus@gmail.com>
2021-09-14 19:49:09 -0700

    09-14-2021

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 from pathlib import Path
 
@@ -163,9 +165,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AWS_ACCESS_KEY_ID = 'AKIAYWY76Y26NB32ETX5'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'quan-enterprise'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
